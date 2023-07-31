15e0ddc53ca1d46229d07db3aff61d019b366d93
ligq <lizaza@lizaza.cn>
2023-07-31 21:21:29 +0800

    追加appid参数!

--- api/wxCallback.go
+++ api/wxCallback.go
@@ -80,8 +80,8 @@ func WxCallback(ctx *gin.Context) {
 
 func SendCustomerServiceMsg(appid, msg, toUser string) error {
 	log.Printf("APPID: %s", appid)
-	url := "http://api.weixin.qq.com/cgi-bin/message/custom/send"
-	//url := "http://api.weixin.qq.com/cgi-bin/message/custom/send?from_appid=" + appid
+	//url := "http://api.weixin.qq.com/cgi-bin/message/custom/send"
+	url := "http://api.weixin.qq.com/cgi-bin/message/custom/send?from_appid=" + appid
 	var data = make(map[string]interface{})
 	data["touser"] = toUser
 	data["msgtype"] = "text"
